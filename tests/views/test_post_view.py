6653dca1fbb5ccbe992aff095aacb090d9d4426c
sidneygyne <sidneyr_gyn@hotmail.com>
2025-11-11 16:54:39 -0300

    add post-views

--- tests/views/test_post_view.py
+++ tests/views/test_post_view.py
@@ -11,5 +11,4 @@ def test_post_view(client):
     response = client.get(url)
     assert response.status_code == 200
 
-
-    assert response.context == 'Hello World'
+    assert response.content == b'Hello World !'
